2bfbd10141fb57d4f5a7c6ac00bb0acb68fe5328
Pradhyuman <superpradhyuman@gmail.com>
2025-06-16 14:08:32 +0530

    generate-session

--- index.ts
+++ index.ts
@@ -2,37 +2,39 @@ import { KiteConnect } from "kiteconnect";
 
 const apiKey = "lq8i0m3my25elb9a";
 const apiSecret = "ghlomv5fpzjk3xyuc16cxg2j7wzghqzp";
-const requestToken = "5lTlKYHa2XhTWQthsdAodfF4b6sS87B3";
-
+// const requestToken = "iaku0m3biz4jqvOc0Ct7XWKPg6EdAB80";
+let accessToken = "n3QatWw9sBvpcq6GCcdCa3oVRKjRRWj6";
 const kc = new KiteConnect({ api_key: apiKey });
 
 console.log(kc.getLoginURL());
 
 async function init() {
   try {
-    await generateSession();
+    // await generateSession();
+    kc.setAccessToken(accessToken);
     await placeOrder();
   } catch (err) {
     console.error(err);
   }
 }
 
-async function generateSession() {
-  try {
-    const response = await kc.generateSession(requestToken, apiSecret);
-    kc.setAccessToken(response.access_token);
-    console.log("Session generated:", response);
-  } catch (err) {
-    console.error("Error generating session:", err);
-  }
-}
+// async function generateSession() {
+//   try {
+//     const response = await kc.generateSession(requestToken, apiSecret);
+//     kc.setAccessToken(response.access_token);
+//     console.log("Session generated:", response);
+//     console.log("Access token:", response.access_token);
+//   } catch (err) {
+//     console.error("Error generating session:", err);
+//   }
+// }
 
 async function placeOrder() {
   try {
-    const order = await kc.placeOrder("amo", {
+    const order = await kc.placeOrder("regular", {
         exchange: "NSE",
         tradingsymbol: "ONGC",
-        transaction_type: "BUY",
+        transaction_type: "SELL",
         quantity: 1,
         product: "CNC",
         order_type: "MARKET"
