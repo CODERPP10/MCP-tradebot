44ba123cb67b89af363068b2db1ab8b7024d4583
Pradhyuman <superpradhyuman@gmail.com>
2025-06-16 14:00:06 +0530

    buy-sell functionality

--- index.ts
+++ index.ts
@@ -1,36 +1,57 @@
 // import { placeOrder } from "./trade";
-import { KiteConnect } from "kiteconnect"; 
+import { McpServer, ResourceTemplate } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { z } from "zod";
+import { placeOrder } from "./trade";
 
+// Create an MCP server
+const server = new McpServer({
+  name: "Demo",
+  version: "1.0.0"
+});
 
-const apiKey = "lq8i0m3my25elb9a";
-const apiSecret = "ghlomv5fpzjk3xyuc16cxg2j7wzghqzp";
-const requestToken = "bZ5coYltgF2I008o8R3RGBg9hPAV3a7R";
-let accessToken = "GOaszQQgFZNkSC7xjCM5RGzMjHSFmv8T";
+// Add an addition tool
+server.tool("add",
+  { a: z.number(), b: z.number() },
+  async ({ a, b }) => ({
+    content: [{ type: "text", text: String(a + b) }]
+  })
+);
 
-const kc = new KiteConnect({ api_key: apiKey });
+// buy a stock
+server.tool(
+  "buy_stock",
+  { stock: z.string(), qty: z.number() },
+  async ({ stock, qty }) => { 
+    // console.log(`Buying ${qty} shares of ${stock}`);
+    placeOrder(stock, qty, "BUY"); 
+    return { content: [{ type: "text", text: `Bought ${qty} shares of ${stock}` }] };
+  }
+);
 
-// placeOrder("ONGC", 1, "SELL");
-// placeOrder("ONGC", 1, "BUY");
+// sell a stock
+server.tool(
+  "sell_stock",
+  { stock: z.string(), qty: z.number() },
+  async ({ stock, qty }) => { 
+    // console.log(`Selling ${qty} shares of ${stock}`);
+    placeOrder(stock, qty, "SELL");
+    return { content: [{ type: "text", text: `Sold ${qty} shares of ${stock}` }] };
+  }
+);
 
-async function init() {
-      try {
-        kc.setAccessToken(accessToken);
-        // await profile();
-        await placeOrder();
-      } catch (err) {
-        console.error(err);
-      }
+// Factor an factorial tool
+server.tool("factorial",
+  { a: z.number() },
+  async ({ a }) => {
+    let result = 1;
+    for (let i = 1; i <= a; i++) {
+      result *= i;
     }
+    return { content: [{ type: "text", text: String(result) }] };
+  }
+);
 
-async function placeOrder() {
-    const loginURL = kc.getLoginURL();
-    console.log(loginURL);
-
-    const response = await fetch(loginURL);
-}
-
-
-    
-// Initialize the API calls
-init();
-
+// Start receiving messages on stdin and sending messages on stdout
+const transport = new StdioServerTransport();
+await server.connect(transport);
--- trade.ts
+++ trade.ts
@@ -2,35 +2,43 @@ import { KiteConnect } from "kiteconnect";
 
 const apiKey = "lq8i0m3my25elb9a";
 const apiSecret = "ghlomv5fpzjk3xyuc16cxg2j7wzghqzp";
-let accessToken = "GOaszQQgFZNkSC7xjCM5RGzMjHSFmv8T";
+// const requestToken = "sOtzCmEDnsZvgxG5ShzaZJ5zQJe3rrHe";
+let accessToken = "n3QatWw9sBvpcq6GCcdCa3oVRKjRRWj6";
 
 const kc = new KiteConnect({ api_key: apiKey });
 
 console.log(kc.getLoginURL());
 
+export async function placeOrder(tradingsymbol: string, quantity: number, type: "BUY" | "SELL") {
+  try {
+    kc.setAccessToken(accessToken)
+    const order = await kc.placeOrder("regular", {
+      exchange: "NSE",
+      tradingsymbol: tradingsymbol,
+      transaction_type: type,
+      quantity: quantity,
+      product: "CNC",
+      order_type: "MARKET"
+    });
+  console.log("Order placed:", order);
+} catch (err) {
+  console.error("Error placing order:", err);
+}
+}
 
-// async function profile() {
+// async function init() {
+//   await generateSession();
+// }
+
+// async function generateSession() {
 //   try {
-//     const profile = await kc.getProfile();
-//     console.log("Profile:", profile);
+//     const response = await kc.generateSession(requestToken, apiSecret);
+//     kc.setAccessToken(response.access_token);
+//     console.log("Session generated:", response);
+//     console.log("Access token:", response.access_token);
 //   } catch (err) {
-//     console.error("Error getting profile:", err);
+//     console.error("Error generating session:", err);
 //   }
 // }
 
-export async function placeOrder( tradingsymbol: string, quantity: number, type: "BUY" | "SELL" ) {
-  try {
-    const order = await kc.placeOrder("amo", {
-        exchange: "NSE",
-        tradingsymbol,
-        transaction_type: type,
-        quantity,
-        product: "CNC",
-        order_type: "MARKET"
-      });
-    console.log("Order placed:", order);
-  } 
-  catch (err) {
-    console.error("Error getting profile:", err);
-  }
-}
+// init();
